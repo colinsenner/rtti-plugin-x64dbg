8628743fa01a9e12923d7265d4a97cdbc5a76a3f
Colin Senner <colin.senner@gmail.com>
2026-05-15 21:29:19 -0700

    fix build

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 
-xcopy /s /q bin\x32\Release\* bin\release\release\x32\plugins\
-xcopy /s /q bin\x64\Release\* bin\release\release\x64\plugins\
+xcopy /s /q bin\x32\Release\* bin\release\x32\plugins\
+xcopy /s /q bin\x64\Release\* bin\release\x64\plugins\
