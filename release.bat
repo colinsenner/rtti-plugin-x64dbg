fe6246543c5a420cc9707a321ff192111a52faa7
Colin Senner <colin.senner@gmail.com>
2026-05-15 19:32:08 -0700

    update release script to include /release directory for easy of unzip

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@
 mkdir bin\release\x32\plugins 2>nul
 mkdir bin\release\x64\plugins 2>nul
 
-xcopy /s /q bin\x32\Release\* bin\release\x32\plugins\
-xcopy /s /q bin\x64\Release\* bin\release\x64\plugins\
+xcopy /s /q bin\x32\Release\* bin\release\release\x32\plugins\
+xcopy /s /q bin\x64\Release\* bin\release\release\x64\plugins\
